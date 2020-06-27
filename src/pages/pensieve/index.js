6b05b9d578ed129431d19698f265c2514ef5d260
akash-suresh <akashsuresh36@gmail.com>
2020-06-27 08:05:53 +0530

    Updated url

--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -124,7 +124,7 @@ const PensievePage = ({ location, data }) => {
     <Layout location={location}>
       <Helmet>
         <title>Pensieve | Akash Suresh</title>
-        <link rel="canonical" href="https://akash-suresh.github.io/pensieve" />
+        <link rel="canonical" href="https://akash-suresh.github.io/v4/pensieve" />
       </Helmet>
 
       <StyledMainContainer>
--- src/templates/post.js
+++ src/templates/post.js
@@ -43,7 +43,7 @@ const PostTemplate = ({ data, location }) => {
     <Layout location={location}>
       <Helmet>
         <title>{title} | Akash Suresh</title>
-        <link rel="canonical" href="https://akash-suresh.github.io/pensieve" />
+        <link rel="canonical" href="https://akash-suresh.github.io/v4/pensieve" />
       </Helmet>
 
       <StyledPostContainer>
